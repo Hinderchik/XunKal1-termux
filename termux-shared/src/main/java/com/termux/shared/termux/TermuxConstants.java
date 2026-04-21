25182b85f0ea52042f11b2ecbeeca7bb7c9dc31d
Hinderchik <gondonloxlp@gmail.com>
2026-04-21 19:42:30 +0300

    Brand: rename to XunKal1 Terminal

--- termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java
+++ termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java
@@ -347,7 +347,7 @@ public final class TermuxConstants {
      */
 
     /** Termux app name */
-    public static final String TERMUX_APP_NAME = "Termux"; // Default: "Termux"
+    public static final String TERMUX_APP_NAME = "XunKal1 Terminal"; // Default: "Termux"
     /** Termux package name */
     public static final String TERMUX_PACKAGE_NAME = "com.termux"; // Default: "com.termux"
     /** Termux GitHub repo name */
